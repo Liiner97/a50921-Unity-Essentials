f464e4d24ca7ed53e825f06cc6e5ac2693878595
Liiner97 <coelhitohg@gmail.com>
2023-03-09 21:24:03 +0000

    3.Increment the scale

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    public string myMessage;
+    public Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,8 @@ public class HelloWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(myMessage);
+        transform.localScale += scaleChange;
+        
         
     }
 }
